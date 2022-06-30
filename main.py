84d884ec3fec4ee3d23519b655bed0195d64a0c6
pickle <you@example.com>
2022-06-29 22:26:18 -0400

    update servers Wed Jun 29 22:26:18 2022

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ URLS = os.path.join(GIT, "URLS.json")
 
 NGROK = "~/python/ngrok"
 
+def pushGit():
+	os.system(f"{GIT} ; git commit . -m \"update servers {time.ctime()}\" ; git push")
 
 
 
@@ -18,9 +20,9 @@ if len(sys.argv) == 3:
 	f = open(URLS, "w")
 	f.write(json.dumps(urls_json))
 	f.close()
-	os.system(f"{GIT} ; git commit . -m \"update servers {time.ctime()}\"")
 
 
+	pushGit()
 
 	print(f"Started server on port {sys.argv[2]}. Use CTRL-Z to kill")
 	ngrok_process = ngrok.get_ngrok_process()
@@ -31,5 +33,8 @@ if len(sys.argv) == 3:
 
 		ngrok.kill()
 
+		del urls_json[sys.argv[1]]
+
+		pushGit()
 else:
 	print("ERROR: use 2 arguments, [app name] [tcp port]")
